09751b6292120bde0f03ecd510cace85292bc66b
Koul <dongik0122@gmail.com>
2024-02-11 18:17:25 +0900

    #12 - 정확한 검색
    정확한 검색은 가능하나 부분 검색은 아직 구현이 되지 않았다.

--- src/main/java/ToMist/SpringBoard/repository/ArticleCommentRepository.java
+++ src/main/java/ToMist/SpringBoard/repository/ArticleCommentRepository.java
@@ -2,9 +2,15 @@ package ToMist.SpringBoard.repository;
 
 import ToMist.SpringBoard.domain.Article;
 import ToMist.SpringBoard.domain.ArticleComment;
+import ToMist.SpringBoard.domain.QArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>
+{
 }
--- src/main/java/ToMist/SpringBoard/repository/ArticleRepository.java
+++ src/main/java/ToMist/SpringBoard/repository/ArticleRepository.java
@@ -1,9 +1,17 @@
 package ToMist.SpringBoard.repository;
 
 import ToMist.SpringBoard.domain.Article;
+import ToMist.SpringBoard.domain.QArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>
+//        QuerydslBinderCustomizer<QArticle>
+{
 }
+
